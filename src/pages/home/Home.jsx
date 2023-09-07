b29291b187b7798b9c311bcbdda3bcf8b2170333
sakibmahbub <arafatsakib48@gmail.com>
2023-09-07 13:46:36 +0600

    added whats popular section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
-      <Trending />
+      <Trending />\
+      <Popular />
     </div>
   );
 };
